62f13f6fd944d22981c3d399b41654c3906a142f
Daniel Altiparmak <Daniel Altiparmak daniel.altiparmak@inquant.de>
2014-12-19 23:56:30 +0100

    skeleton for pymite subcommand "configure"

--- pymite/cmd/__init__.py
+++ pymite/cmd/__init__.py
@@ -8,13 +8,44 @@
 __author__ = 'Daniel Altiparmak <daniel.altiparmak@inquant.de>'
 __docformat__ = 'plaintext'
 
+
 import click
 
+try:
+    range_type = xrange
+except NameError:
+    range_type = range
 
-@click.command()
-def cli():
-    print('Hello World')
 
+class Config(object):
+
+    def __init__(self):
+        self.verbose = False
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+@click.group()
+@click.option('--verbose', is_flag=True)
+@pass_config
+def cli(config, verbose):
+    config.verbose = verbose
+
+
+@cli.command()
+@click.argument('name')
+@click.argument('api_key')
+@pass_config
+def configure(config, name, api_key):
+    '''configure your account to use pymite.
+    This creates a .pymite file in your home folder.
+    If file already exists, those config options get overwritten
+    '''
+    if config.verbose:
+        click.echo('We are in verbose mode')
+    click.echo(name)
+    click.echo(api_key)
 
 # vim: set ft=python ts=4 sw=4 expandtab :
 
