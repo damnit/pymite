8c40d668b78b22af15d4c3564b9c4533d4c5c4e7
Otto Hockel <hockel.otto@gmail.com>
2015-01-06 21:57:01 +0100

    made 'python setup.py test' work with pytests, adding tox.ini

--- setup.py
+++ setup.py
@@ -8,7 +8,28 @@ __author__ = 'Daniel Altiparmak <daniel.altiparmak@inquant.de>'
 __docformat__ = 'plaintext'
 
 
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -21,10 +42,13 @@ setup(
         'Click',
         'colorama',
     ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
     entry_points='''
         [console_scripts]
         pymite=pymite.cmd:cli
     '''
 )
+
 
 # vim: set ft=python ts=4 sw=4 expandtab :
