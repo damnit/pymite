84477740681cdb53372f1647000587c979cebd95
Otto Hockel <hockel.otto@gmail.com>
2015-01-17 21:52:36 +0100

    services all test

--- pymite/test/test_services_adapter.py
+++ pymite/test/test_services_adapter.py
@@ -12,10 +12,30 @@ from pymite.api.adapters import Services
 
 def test_services_setup(libfactory):
     """ Test tracker setup. """
-    factory_services = libfactory.projects_adapter
-    assert factory_services is not None
-    services = Services(factory_projects.realm, factory_projects.apikey)
+    factory = libfactory.services_adapter
+    assert factory is not None
+    services = Services(factory.realm, factory.apikey)
     assert services.adapter == 'services'
+
+
+def test_services_all(monkeypatch, libfactory):
+    services = libfactory.services_adapter
+    all_data = [
+        {'service': {'id': 1, 'archived': False, 'billable': True,
+                     'note': 'foo', 'name': 'consulting', 'hourly_rate': 3300,
+                     'created_at': '2014-12-12T13:37:37+01:00',
+                     'updated_at': '2014-12-13T13:37:37+01:00',}},
+        {'service': {'id': 2, 'archived': False, 'billable': True,
+                     'note': 'bar', 'name': 'testing', 'hourly_rate': 4200,
+                     'created_at': '2014-12-12T13:37:37+01:00',
+                     'updated_at': '2014-12-13T13:37:37+01:00',}},
+    ]
+    urlopen_services = mock_urlopen(all_data)
+    monkeypatch.setattr(urllib.request, 'urlopen', urlopen_services)
+
+    all_services = services.all()
+    assert all_services == list(map(lambda x: x['service'], all_data))
+    assert len(all_services) == len(all_data)
 
 
 # vim: set ft=python ts=4 sw=4 expandtab :
